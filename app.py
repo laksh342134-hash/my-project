33efecfa89f31ee601f37f19cd65b6102259c4ee
laksh342134-hash <laksh.342134@gmail.com>
2026-09-16 01:09:02 +0530

    Prepare app for deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 import pandas as pd
 from flask import Flask, render_template, request
@@ -86,4 +87,8 @@ def demo():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", "5000")),
+        debug=False,
+    )
